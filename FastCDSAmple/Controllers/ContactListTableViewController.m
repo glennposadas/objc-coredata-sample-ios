1a1f836aed0cd0ae52992f0128f8b8186fa0d716
Glenn Posadas <hello@glennvon.com>
2021-02-04 02:41:19 +0800

    add search feature.

--- FastCDSAmple/Controllers/ContactListTableViewController.m
+++ FastCDSAmple/Controllers/ContactListTableViewController.m
@@ -14,21 +14,74 @@
 #import "ContactListTableViewController.h"
 @import CoreData;
 
-@interface ContactListTableViewController ()
+@interface ContactListTableViewController () <UISearchBarDelegate>
 
 typedef NSArray<Contact *> ContactArray;
 @property (strong, nonatomic) ContactArray *contacts;
 @property (strong, nonatomic) AppDelegate *appDelegate;
 @property (strong, nonatomic) NSManagedObjectContext *bgContext;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property(nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
 @end
 
 @implementation ContactListTableViewController
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+    [self fetchData];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    dispatch_async(dispatch_get_main_queue(), ^(void) {
+        if(searchText) {
+            NSMutableArray *subPredicates = [[NSMutableArray alloc] init];
+            [subPredicates addObject:[NSPredicate predicateWithFormat: @"name CONTAINS[cd] %@", searchText]];
+            [subPredicates addObject:[NSPredicate predicateWithFormat: @"details.email CONTAINS[cd] %@", searchText]];
+            [subPredicates addObject:[NSPredicate predicateWithFormat: @"details.phone CONTAINS[cd] %@", searchText]];
+            
+            NSPredicate *predicate = [NSCompoundPredicate orPredicateWithSubpredicates:subPredicates];
+            
+            NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Contact"];
+            request.predicate = predicate;
+            
+            __block NSArray *array;
+            [self->_bgContext performBlockAndWait:^{
+                NSError *error;
+                array = [self->_bgContext executeFetchRequest:request error:&error];
+            }];
+            
+            self.contacts = array;
+            dispatch_async(dispatch_get_main_queue(), ^
+                           {
+                [self.tableView reloadData];
+            });
+            
+        }
+    });
+}
+
+- (void)fetchData {
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Contact"];
+    
+    __block NSArray *array;
+    [_bgContext performBlockAndWait:^{
+        NSError *error;
+        array = [_bgContext executeFetchRequest:request error:&error];
+    }];
+    
+    self.contacts = array;
+    dispatch_async(dispatch_get_main_queue(), ^
+                   {
+        [self.tableView reloadData];
+    });
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.appDelegate = (AppDelegate *)UIApplication.sharedApplication.delegate;
     self.bgContext = self.appDelegate.persistentContainer.newBackgroundContext;
+    self.searchBar.showsCancelButton = YES;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -58,29 +111,14 @@ typedef NSArray<Contact *> ContactArray;
     
     [newContact setValue:details forKey:@"details"];
     
-    [self.appDelegate saveContext];
+    NSError *error = nil;
+    [self.bgContext save:&error];
     [self fetchData];
-}
-
-- (void)fetchData {
-    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Contact"];
-    
-      __block NSArray *array;
-    [_bgContext performBlockAndWait:^{
-      NSError *error;
-        array = [_bgContext executeFetchRequest:request error:&error];
-    }];
-    
-    self.contacts = array;
-    dispatch_async(dispatch_get_main_queue(), ^
-    {
-        [self.tableView reloadData];
-    });
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     ContactTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ContactTableViewCell"];
-
+    
     NSUInteger row = indexPath.row;
     
     if (self.contacts.count > row) {
